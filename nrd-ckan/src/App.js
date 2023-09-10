1c168f20cd4fe9df0f20f04ebc5aa4a6885ca4d7
gabrielc42 <clark.gabe97@gmail.com>
2023-09-09 21:13:35 -0500

    test api return success update

--- nrd-ckan/src/App.js
+++ nrd-ckan/src/App.js
@@ -1,4 +1,7 @@
 
+// console log in react webpage returns success on test api pull
+// i don't remember if it did this 6 months ago, but this is a good start
+
 function App() {
   fetch("https://demo.ckan.org/api/3/action/group_list")
     .then((response) => response.json())
